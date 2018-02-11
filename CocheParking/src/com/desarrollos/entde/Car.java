66295891e0dbc1fc8701c41a5781cd8f1d88e4f1
Jose <Jose@DESKTOP-IEGREQ2>
2018-02-11 20:53:01 +0100

    a

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -32,7 +32,7 @@ public class Car
         slowDown();
         moveDown();
         accelerate();
-        car.printGarage();
+        g.printGarage();
 //        turnRight();
 //        turnHeadlightsOn();
 //        turnHeadlightsOff();
