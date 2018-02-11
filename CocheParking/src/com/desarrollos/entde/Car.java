6b1a7d4084555af3f2b853c7860d74451b7b55a7
Jose <Jose@DESKTOP-IEGREQ2>
2018-02-11 20:44:32 +0100

    subida

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -1,6 +1,5 @@
 package com.desarrollos.entde;
 
-import java.util.Arrays;
 import java.awt.Color;
 /**
  * Class that models objects of type "car"
@@ -29,7 +28,7 @@ public class Car
     public static void main(String[] args) throws InterruptedException{
     	Garage g = new Garage();
     	Car car = new Car(g);
-        car.accelerate();
+        Car.accelerate();
         slowDown();
         moveDown();
         accelerate();
@@ -39,11 +38,15 @@ public class Car
 //        turnHeadlightsOff();
     }    
 
-    /**
+    private void printGarage() {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+	/**
      * Accelerate the car
      * @throws InterruptedException 
      */
-    public void accelerate() throws InterruptedException{
+    public static void accelerate() throws InterruptedException{
         String c = new String("*");
         String space = new String(" ");
     	for(int i=0; i<5; i++)
@@ -58,8 +61,8 @@ public class Car
      * Slow down the car
      * @throws InterruptedException 
      */
-    public void slowDown() throws InterruptedException{
-    	string c = new string("     *");
+    public static void slowDown() throws InterruptedException{
+    	String c = new String("     *");
     	for(int i=0; i<5; i++)
         {
         	System.out.print(c);
@@ -73,9 +76,9 @@ public class Car
      * Move the car downwards
      * @throws InterruptedException 
      */    
-    public void moveDown() throws InterruptedException{
-    	string c = new string("\t\t\t\t  *"));
-    	for(int i=0, i<5, i++)
+    public static void moveDown() throws InterruptedException{
+    	String c = new String("\t\t\t\t  *");
+    	for(int i=0; i<5; i++)
         {
     		if(i!=4)
     			System.out.println(c);
